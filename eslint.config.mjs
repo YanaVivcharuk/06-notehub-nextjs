2e8113f47aad6267fac062084df9392c1bb384a6
YanaVivcharuk <yanavivcharuk@icloud.com>
2025-08-29 20:33:33 +0200

    first-next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,11 +12,8 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "prettierConfig"
-  ),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  prettierConfig,
 ];
 
 export default eslintConfig;
